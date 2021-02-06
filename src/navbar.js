93990173822b73e75e53bf0236018a035a4fe03a
Lea Weissman <lea.weissman@icloud.com>
2021-02-06 13:31:23 +0100

    lea is having fun with the nav bar

--- src/navbar.js
+++ src/navbar.js
@@ -1,5 +1,20 @@
-const activeicon = document.querySelector('.jsisfun')
+//const activeincons = document.querySelectorAll('.icon')
 
-activeicon.addEventListener('click', () => {
-    activeicon.classList.add('home');
-})
+//activeincons.forEach((activeicon) => {
+//activeicon.addEventListener('click', () => {
+//activeicon.classList.toggle('nav_container img');
+//activeicon.classList.toggle('activeicons');
+//})
+//})
+
+
+
+//const icons = document.querySelectorAll('.icon')
+//icons.forEach(toggleicon);
+
+//function toggleicon(activeiconfun) {
+    //activeiconfun.addEventListener('click', () => {
+        //const activeicons = document.querySelectorAll('.icon');
+        //activeicons.classList.toggle('activeicons');
+    //})
+//}
